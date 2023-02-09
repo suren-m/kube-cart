761a5e21062ef033b8b48f46429f18c97ce877e5
suren <suren>
2023-02-09 18:14:00 +0000

    Migrate: Go based chat app prototype

---
+++ prototypes/websockets_chat/app/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
+)
+
+type User struct {
+	ID   int
+	name string
+}
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+	// allow all origin
+	// m.Upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	activeUsers := make(map[*melody.Session]*User)
+	lock := new(sync.Mutex)
+	counter := 0
+
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "index.html")
+	})
+
+	r.GET("/cpu", func(c *gin.Context) {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println("still running")
+		}
+	})
+
+	r.GET("/host", func(c *gin.Context) {
+		activeSessions, _ := m.Sessions()
+		res := getHost() + " (" + strconv.Itoa(len(activeSessions)) + " active sessions)"
+		c.String(http.StatusOK, res)
+	})
+
+	r.GET("/connections", func(c *gin.Context) {
+		activeSessions, _ := m.Sessions()
+		c.String(http.StatusOK, strconv.Itoa(len(activeSessions)))
+	})
+
+	r.GET("/ready", func(c *gin.Context) {
+		activeSessions, _ := m.Sessions()
+		res := len(activeSessions)
+		// demo threshold
+		if res <= 3 {
+			c.String(http.StatusOK, "success")
+		} else {
+			c.String(http.StatusTooManyRequests, "failure")
+		}
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleConnect(func(s *melody.Session) {
+		lock.Lock()
+		counter += 1
+		activeUsers[s] = &User{counter, "User " + strconv.Itoa(counter)}
+		s.Write([]byte("Connected to: " + getHost() + " (" + strconv.Itoa(len(activeUsers)) + " active members)"))
+		lock.Unlock()
+	})
+
+	m.HandleDisconnect(func(s *melody.Session) {
+		lock.Lock()
+		m.BroadcastOthers([]byte("Disconnected: "+activeUsers[s].name), s)
+		delete(activeUsers, s)
+		lock.Unlock()
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		lock.Lock()
+		prefix := []byte("<" + activeUsers[s].name + "> ")
+		res := append(prefix, msg...)
+		m.Broadcast(res)
+		lock.Unlock()
+	})
+
+	// m.HandleMessage(func(s *melody.Session, msg []byte) {
+	// 	m.Broadcast(msg)
+	// })
+
+	r.Run(":5000")
+}
+
+func getHost() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "not available"
+	}
+
+	nodename := os.Getenv("NODE_NAME")
+	result := hostname + " on " + nodename
+
+	return result
+}
